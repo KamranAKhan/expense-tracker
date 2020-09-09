6654fc90bb2c8ab826351713dc461808b5bdfa36
Kamran Ahmed Khan <code.kamran@gmail.com>
2020-09-09 22:04:45 +0500

    update icons url for new deployment

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,16 @@ var urlsToCache = [
     // '/static/js/2.0beda9f7.chunk.js', // for build
     // '/static/js/main.dbce4115.chunk.js', // for build
     // '/static/css/main.8df5b9e0.chunk.css', // for build    
-    '/favicon.ico',
+    '/icons/android-icon-36x36.png',
+    '/icons/android-icon-48x48.png',
+    '/icons/android-icon-72x72.png',
+    '/icons/android-icon-96x96.png',
+    '/icons/android-icon-144x144.png',
+    '/icons/android-icon-192x192.png',
+    '/icons/favicon.ico',
+    '/icons/favicon-32x32.png',
+    '/icons/favicon-96x96.png',
+    '/icons/favicon-16x16.png',    
     '/manifest.json',    
     '/logo192.png',    
     '/',
